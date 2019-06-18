0a5dfa35065bf52e46d2cc3d051de9c07afe96a6
HbbTV Development <faridrdev@gmail.com>
2019-06-18 20:04:59 +0430

    working on routes

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\App as App;
 
 require 'vendor/slim/autoload.php';
 
@@ -21,21 +20,25 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $response;
 });
 
-$app->group('/users/{id:[0-9]+}', function (App $app) {
-    $app->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, $args) {
-        // Find, delete, patch or replace user identified by $args['id']
-    })->setName('user');
-    $app->get('/reset-password', function ($request, $response, $args) {
-        // Route for /users/{id:[0-9]+}/reset-password
-        // Reset the password for user identified by $args['id']
-    })->setName('user-password-reset');
-    $app->post('/ticket/new', function (Request $request, Response $response) {
-        $data = $request->getParsedBody();
-        $ticket_data = [];
-        $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
-        $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
-    });
-});
+foreach (glob('routes/*.php') as $filename) {
+    require $filename;
+}
+
+//$app->group('/users/{id:[0-9]+}', function (App $app) {
+//    $app->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, $args) {
+//        // Find, delete, patch or replace user identified by $args['id']
+//    })->setName('user');
+//    $app->get('/reset-password', function ($request, $response, $args) {
+//        // Route for /users/{id:[0-9]+}/reset-password
+//        // Reset the password for user identified by $args['id']
+//    })->setName('user-password-reset');
+//    $app->post('/ticket/new', function (Request $request, Response $response) {
+//        $data = $request->getParsedBody();
+//        $ticket_data = [];
+//        $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
+//        $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
+//    });
+//});
 
 
 $app->run();
--- routes/news.php
+++ routes/news.php
@@ -1,2 +1,23 @@
 <?php
 
+use Slim\App as App;
+
+$app->group('/news', function (App $app) {
+
+    $app->map(['GET'], '', function ($request, $response, $args) {
+        $response->getBody()->write('all news');
+    })->setName('news-all');
+
+    $app->get('/{id:[0-9]+}', function ($request, $response, $args) {
+        // Route for /news/{id:[0-9]+}
+        // Reset the password for user identified by $args['id']
+        $response->getBody()->write('news details for item with id: ' . $args['id']);
+    })->setName('user-password-reset');
+
+    $app->post('/ticket/new', function (Request $request, Response $response) {
+        $data = $request->getParsedBody();
+        $ticket_data = [];
+        $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
+        $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
+    });
+});
