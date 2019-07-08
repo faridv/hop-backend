4fe306c3febfd550848e04534859934ead16f0a9
Farid Rn <faridv@gmail.com>
2019-07-09 00:25:18 +0430

    implemented default module router importer

---
+++ routes/modules/default.php
@@ -0,0 +1,20 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('{"success": true, "message": "HbbTV Backend"}');
+
+    return $response;
+});
+
+$c = new \Slim\Container(); //Create Your container
+//Override the default Not Found Handler after App
+unset($app->getContainer()['notFoundHandler']);
+$app->getContainer()['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        $response = new \Slim\Http\Response(404);
+        return $response->write('{"success":false,"message":"endpoint not found."}');
+    };
+};
