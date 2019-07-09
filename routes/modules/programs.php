bf5de6e9d67cad94e561cca646b10e22feaa12d1
HbbTV Development <faridrdev@gmail.com>
2019-07-09 18:32:02 +0430

    added episode load and added url trailing slash support

--- routes/modules/programs.php
+++ routes/modules/programs.php
@@ -8,14 +8,23 @@ use Slim\App as App;
 
 $app->group('/programs', function (App $app) {
 
-    $app->map(['GET'], '', function (Request $request, Response $response, $args) {
+    // Get programs list
+    $app->map(['GET'], '[/]', function (Request $request, Response $response, $args) {
         $programs = ProgramHandler::getInstance()->get();
         return $response->withJson($programs, 200, JSON_UNESCAPED_UNICODE);
     })->setName('programs');
 
-    $app->map(['GET'], '/list', function (Request $request, Response $response, $args) {
+    // Get programs list [2]
+    $app->map(['GET'], '/list[/]', function (Request $request, Response $response, $args) {
         $programs = ProgramHandler::getInstance()->getList();
         return $response->withJson($programs, 200, JSON_UNESCAPED_UNICODE);
     })->setName('programs-list');
+
+    // Get program episodes by program id
+    $app->map(['GET'], '/{programId:[0-9]+}[/]', function (Request $request, Response $response, $args) {
+        $programId = $args['programId'];
+        $programs = ProgramHandler::getInstance()->getEpisodes($programId);
+        return $response->withJson($programs, 200, JSON_UNESCAPED_UNICODE);
+    })->setName('program-episodes');
 
 });
