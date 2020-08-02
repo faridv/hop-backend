6294bd10de94a392c487403395bc613e1b54b862
Farid Rn <faridv@gmail.com>
2020-08-02 13:07:06 +0430

    added id support to news service

--- handlers/news.php
+++ handlers/news.php
@@ -21,8 +21,8 @@ final class NewsHandler {
         return self::$instance;
     }
 
-    private function checkCache() {
-        return $this->cacheClient->get('news');
+    private function checkCache($key = 'news') {
+        return $this->cacheClient->get($key);
     }
 
     public function getAll() {
@@ -35,6 +35,18 @@ final class NewsHandler {
         $cachedData = $this->cacheClient->set('news', json_encode($data, JSON_UNESCAPED_UNICODE), $this->config->expire);
         return Response::prepare($cachedData, $this->fromCache);
     }
+	
+	public function getCategory($newsId) {
+		$cachedData = $this->checkCache('news-' . $newsId);
+        if ($cachedData) {
+            $this->fromCache = true;
+            return Response::prepare($cachedData, $this->fromCache);
+        }
+        $url = str_ireplace('Categories=-1', 'Categories=' . $newsId, $this->config->url);
+        $data = $this->handle(Proxy::fetch($url));
+        $cachedData = $this->cacheClient->set('news-' . $newsId, json_encode($data, JSON_UNESCAPED_UNICODE), $this->config->expire);
+        return Response::prepare($cachedData, $this->fromCache);
+	}
 
     private function handle($items) {
         $output = [];
@@ -57,6 +69,15 @@ final class NewsHandler {
                 $c->thumbnail = new stdClass();
                 $c->thumbnail->url = $item->Repositories[0]->Thumbnail;
                 $c->thumbnail->desc = $item->Repositories[0]->Description;
+				$c->media = null;
+				
+				if (count($item->Repositories) > 1) {
+					foreach($item->Repositories as $repo) {
+						if (stristr($repo->FilePath, '.mp4')) {
+							$c->media = str_replace('\\', '/', str_replace('.jpg', '_wlq.mp4', $repo->Thumbnail));
+						}
+					}
+				}
             }
             $output[] = $c;
         }
--- routes/modules/news.php
+++ routes/modules/news.php
@@ -9,9 +9,14 @@ use Slim\App as App;
 $app->group('/news', function (App $app) {
 
     // all news
-    $app->map(['GET'], '', function (Request $request, Response $response, $args) {
+    $app->map(['GET'], '[/]', function (Request $request, Response $response, $args) {
         $news = NewsHandler::getInstance()->getAll();
         return $response->withJson($news, 200, JSON_UNESCAPED_UNICODE);
     })->setName('news-all');
+	
+	$app->map(['GET'], '/{id:[0-9,]+}[/]', function (Request $request, Response $response, $args) {
+        $surah = NewsHandler::getInstance()->getCategory($args['id']);
+        return $response->withJson($surah, 200, JSON_UNESCAPED_UNICODE);
+    })->setName('news-item');
 
 });
