e8278aa19499e4b273d5e9107311fa443364820b
Farid Rn <faridv@gmail.com>
2020-12-27 20:48:41 +0330

    implemented items route and handler

---
+++ handlers/items.php
@@ -0,0 +1,47 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use ResponseHelper as Response;
+
+final class ItemsHandler {
+
+    private static $instance = null;
+    private $config;
+    private $cacheClient;
+    private $fromCache = false;
+
+    private function __construct() {
+        $this->config = Config::getInstance()->data->modules->{'items'};
+        $this->cacheClient = Cache::getInstance();
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new ItemsHandler();
+        }
+        return self::$instance;
+    }
+
+    private function checkCache($key = 'items') {
+        return $this->cacheClient->get($key);
+    }
+
+    public function getAll(Request $request, $args) {
+        $uri = isset($args['uri']) && $args['uri'] ? $args['uri'] : '';
+		$uri .= '?' . $request->getUri()->getQuery();
+        $cacheKey = str_replace('/', '$', $uri);
+        $cachedData = $this->checkCache('items' . $cacheKey);
+        if ($cachedData) {
+            $this->fromCache = true;
+            return Response::prepare($cachedData, $this->fromCache);
+        }
+        $data = $this->handle(Proxy::fetch($this->config->url . $uri));
+        $cachedData = $this->cacheClient->set($cacheKey, json_encode($data, JSON_UNESCAPED_UNICODE), $this->config->expire);
+        return Response::prepare($cachedData, $this->fromCache);
+    }
+
+    private function handle($items) {
+        return $items;
+    }
+
+}
---
+++ routes/modules/items.php
@@ -0,0 +1,16 @@
+<?php
+
+require BASE . DS . 'handlers/items.php';
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\App as App;
+
+$app->group('/items', function (App $app) {
+
+    $app->map(['GET'], '[/{uri:.*}]', function (Request $request, Response $response, $args) {
+        $items = ItemsHandler::getInstance()->getAll($request, $args);
+        return $response->withJson($items, 200, JSON_UNESCAPED_UNICODE);
+    })->setName('items');
+
+});
