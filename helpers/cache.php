95482cb5ee51f1cafa4b46e574214ccc0a0842c3
Farid Rn <faridv@gmail.com>
2021-04-07 00:33:09 +0430

    fixed cache bugs

--- helpers/cache.php
+++ helpers/cache.php
@@ -13,9 +13,14 @@ final class Cache {
             'scheme' => 'tcp',
             'host' => $this->config->host,
             'port' => $this->config->port,
-//            'database' => $this->config->database,
+            'database' => $this->config->database,
         ];
-        $this->client = new Predis\Client($this->serverConfig);
+        try {
+            $this->client = new Predis\Client($this->serverConfig);
+        } catch (Exception $exception) {
+            // ignore
+            $this->client = null;
+        }
     }
 
     public static function getInstance() {
@@ -26,19 +31,27 @@ final class Cache {
     }
 
     public function set($key, $data, $expire) {
-        try {
-            if ($expire !== -1)
-                $this->client->set($key, $data, 'EX', $expire);
-            else
-                $this->client->set($key, $data);
-        } catch (Exception $e) {
-            // ignore
+        if ($this->client) {
+            try {
+                if ($expire !== -1)
+                    $this->client->set($key, $data, 'EX', $expire);
+                else
+                    $this->client->set($key, $data);
+            } catch (Exception $exception) {
+                // ignore
+            }
         }
         return is_string($data) ? json_decode($data) : $data;
     }
 
     public function get($key) {
-        $data = $this->client->get($key);
+        $data = '';
+        try {
+            $data = $this->client->get($key);
+        } catch (Exception $exception) {
+            // ignore
+        }
+//        $data = $this->client !== null ?  : '';
         if (strlen($data) === 0)
             return $data;
         return is_string($data) ? json_decode($data) : $data;
